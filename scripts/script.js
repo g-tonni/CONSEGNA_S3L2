46ac7555085b69b22f80fe2748248c3a4ab9e357
Giada <tonni.giada.210802@gmail.com>
2025-10-07 16:44:38 +0200

    ESERCIZIO NON FINITO

--- scripts/script.js
+++ scripts/script.js
@@ -2,49 +2,118 @@
        Scrivi una funzione per cambiare il titolo della pagina in qualcos'altro
     */
 
-const changeTitle = function () {}
+const h1 = document.getElementsByTagName('h1')
+console.log(h1)
+const title = 'Ciao, sono Giada'
+
+const changeTitle = function (titolo, testo) {
+  titolo[0].innerText = testo
+}
+
+changeTitle(h1, title)
 
 /* ESERCIZIO 2
        Scrivi una funzione per aggiungere al titolo della pagina una classe "myHeading"
     */
 
-const addClassToTitle = function () {}
+const classe = 'myHeading'
+
+const addClassToTitle = function (titolo, nomeClasse) {
+  titolo[0].setAttribute('class', nomeClasse)
+}
+
+addClassToTitle(h1, classe)
 
 /* ESERCIZIO 3
        Scrivi una funzione che cambi il testo dei p figli di un div
       */
 
-const changePcontent = function () {}
+const p = document.querySelectorAll('div p')
+console.log(p)
+const contenuto = 'Ho cambiato questo testo!'
+
+const changePcontent = function (par, cont) {
+  for (let i = 0; i < par.length; i++) {
+    par[i].innerText = cont
+  }
+}
+
+changePcontent(p, contenuto)
 
 /* ESERCIZIO 4
        Scrivi una funzione che cambi la proprietà href di ogni link (tranne quello nel footer) con il valore https://www.google.com
       */
 
-const changeUrls = function () {}
+const linkGoogle = 'https://www.google.com'
+const linkPage = document.getElementsByTagName('a')
+console.log(linkPage)
+
+/* const changeUrls = function (a, link, b) {
+  for (let i = 0; i < a.length; i++) {
+    a[i].closest(b)
+    if (b !== 'footer') {
+      a.setAttribute('href', link)
+    }
+  }
+} */
+
+//changeUrls(linkPage, linkGoogle)
 
 /* ESERCIZIO 5
        Scrivi una funzione che aggiunga un nuovo elemento lista alla seconda lista non ordinata
     */
 
-const addToTheSecond = function () {}
+const secondList = document.querySelectorAll('body ul')[1]
+console.log(secondList)
+const quarto = '4th'
+
+const addToTheSecond = function (lista, testo) {
+  const nuovoLi = document.createElement('li')
+  nuovoLi.innerText = testo
+  lista.appendChild(nuovoLi)
+}
+addToTheSecond(secondList, quarto)
 
 /* ESERCIZIO 6
        Scrivi una funzione che aggiunga un paragrafo al primo div
     */
 
-const addParagraph = function () {}
+const primoDivP = document.querySelector('body div')
+console.log(primoDivP)
+const testoP = 'Questo è un nuovo testo AGGIUNTO CON UNA FUNZIONE'
+
+const addParagraph = function (div, testo) {
+  const nuovoP = document.createElement('p')
+  nuovoP.innerText = testo
+  div.appendChild(nuovoP)
+}
+addParagraph(primoDivP, testoP)
 
 /* ESERCIZIO 7
-       Scrivi una funzione che faccia scomparire la prima lista non ordinata
-    */
+Scrivi una funzione che faccia scomparire la prima lista non ordinata
+*/
 
-const hideFirstUl = function () {}
+const primaLista = document.querySelector('body ul')
+console.log(primaLista)
+
+const hideFirstUl = function (lista) {
+  lista.remove()
+}
+hideFirstUl(primaLista)
 
 /* ESERCIZIO 8 
-       Scrivi una funzione che renda verde il background di ogni lista non ordinata
-      */
+Scrivi una funzione che renda verde il background di ogni lista non ordinata
+*/
 
-const paintItGreen = function () {}
+const ul = document.querySelectorAll('ul')
+console.log(ul)
+
+const paintItGreen = function (lista) {
+  for (let i = 0; i < lista.length; i++) {
+    lista[i].style.backgroundColor = 'green'
+  }
+}
+paintItGreen(ul)
 
 /* ESERCIZIO 9
        Scrivi una funzione che rimuova l'ultima lettera dall'h1 ogni volta che l'utente lo clicca
@@ -63,7 +132,18 @@ const revealFooterLink = function () {}
        La tabella avrà 5 elementi e questa struttura: immagine, nome prodotto, quantità, prezzo
     */
 
-const generateTable = function () {}
+const area = document.getElementById('tableArea')
+
+const generateTable = function (area, nR, nD) {
+  const table = document.createElement('table')
+  area.innerHTML = 'table'
+  for (let i = 0; i < nR; i++) {
+    const tr = document.createElement('tr')
+    for (let j = 0; j < nD; j++) {
+      const td = document.createElement('td')
+    }
+  }
+}
 
 /* ESERCIZIO 12
        Crea una funzione che aggiunga una riga alla tabella precedentemente creata e fornisca i dati necessari come parametri
